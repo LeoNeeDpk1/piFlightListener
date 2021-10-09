0067288c257448259b19438e1ea27dd2e202600b
LeoNeeD <leoneedpk1@gmail.com>
2021-10-09 13:54:24 +1200

    rep created

---
+++ bindings.py
@@ -0,0 +1,38 @@
+#1 = ON, 0 = OFF
+#Example: 40 = Alternator off
+
+#PINS:
+#1 Master
+#4 Alternator
+#5 Avionics
+#6 Parking break
+#7 Nav lights
+#8 Beacon lights
+#9 Strobe lights
+#10 Taxi lights
+#11 Landing lights
+#12 Pitot heat
+#13 AP button
+#14 APPR button
+#15 Landing gear
+#T+\- Thrust UP\DOWN
+#M+\- Mixture UP\DOWN
+#P+\- Propeller UP\DOWN
+#A+\- Altitude UP\DOWN
+#AB Altitude AP button
+#H+\- Heading UP\DOWN
+#HB Heading AP button
+#V+\- VS UP\DOWN
+#VB - VS AP button
+#N+\- Nav freq UP\DOWN
+#NB ...
+#C+\- Com freq UP\DOWN
+#CB ...
+#B+\- Barometer UP\DOWN
+#BB barometer push button (STD Baro)
+
+bindings = {
+'11': '66',
+'10': 'SHIFT_66',
+'41': '77'
+}
---
+++ listener.py
@@ -0,0 +1,23 @@
+import memcache, time, os, subprocess, bindings
+shared = memcache.Client(['192.168.1.120:11211'], debug=0)
+prev_data = shared.get('input')
+
+bindings = bindings.bindings
+
+while True:
+    data = shared.get('input')
+    if not prev_data == data:
+        prev_data = data
+        try:
+            key = bindings[data[8:]]
+            print(key)
+            try:
+                print('python.exe', 'D:\Code\keysender\keypress.py', key)
+                subprocess.Popen(['python.exe', 'D:\Code\keysender\keypress.py', key], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            except:
+                print("Something wrong with keypress emulate...")
+        except:
+            print("There is no binding for " + data[8:])
+        
+
+    time.sleep(0.01)
